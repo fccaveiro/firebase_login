8a69f13b294798a4e6fdfbda63cdd09a12f751c4
pbreda <vbmaster-koji@hotmail.com>
2018-07-29 16:21:46 +0100

    upload total

---
+++ auth_conf.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyDRYTxqTZGK6u6E1l9o1ceyzobgDhpFI4E",
+    authDomain: "bootcamp-c83a2.firebaseapp.com",
+    databaseURL: "https://bootcamp-c83a2.firebaseio.com",
+    projectId: "bootcamp-c83a2",
+    storageBucket: "bootcamp-c83a2.appspot.com",
+    messagingSenderId: "690670014363"
+  };
